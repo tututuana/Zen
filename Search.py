05ab9b187c44f51cce937141416e8e08a1aa5df6
tututuana <51187395+tututuana@users.noreply.github.com>
2021-07-07 20:50:47 +0300

    0.8 Rewrite Thx to hoffstadt(dearpygui)

--- Search.py
+++ Search.py
@@ -1,6 +1,4 @@
 import datetime
-from dearpygui.core import *
-from dearpygui.simple import *
 import json
 import os
 import PyDictionary
@@ -15,6 +13,7 @@ import wikipedia
 import win10toast
 import wolframalpha
 from googlesearch import search
+import dearpygui.dearpygui as dpg
 
 functionalityData = json.load(open("data/functionalityData.json"))
 preprocessingData = json.load(open("data/preprocessingData.json"))
@@ -22,158 +21,164 @@ preprocessingData = json.load(open("data/preprocessingData.json"))
 dictionary = PyDictionary.PyDictionary()
 wolfram = wolframalpha.Client(functionalityData["wolfram_key"])
 
-def searchZen (query):
-	# Query Pre-processing
+# generate uuids (just to go with your existing code)
+query_id = dpg.generate_uuid()
+output_id = dpg.generate_uuid()
 
-	query = query.lower()
 
-	for contraction, expansion in preprocessingData["contractions"].items():
-		query = query.replace(contraction, expansion)
+def searchZen(query):
+    # Query Pre-processing
 
-	query = re.sub(r'[^\w\s]','', query)
+    query = query.lower()
 
-	# Antonyms, Definitions, Synonyms, and Translations
+    for contraction, expansion in preprocessingData["contractions"].items():
+        query = query.replace(contraction, expansion)
 
-	if query.split()[0] == "antonym":
-		query = query.replace("antonym ", "", 1)
-		query = query.replace(" ", "")
+    query = re.sub(r'[^\w\s]', '', query)
 
-		antonyms = dictionary.antonym(query)
-		output = ""
+    # Antonyms, Definitions, Synonyms, and Translations
 
-		for antonym in antonyms:
-			output += antonym + "\n"
-		
-		return output
+    if query.split()[0] == "antonym":
+        query = query.replace("antonym ", "", 1)
+        query = query.replace(" ", "")
 
-	if query.split()[0] == "define":
-		query = query.replace("define ", "")
-		query = query.replace(" ", "")
+        antonyms = dictionary.antonym(query)
+        output = ""
 
-		returnData = dictionary.meaning(query)
-		output = ""
+        for antonym in antonyms:
+            output += antonym + "\n"
 
-		for type, definition in returnData.items():
-			for singleDefinition in definition:
-				output += type + " | " + singleDefinition + "\n"
+        return output
 
-		return output
+    if query.split()[0] == "define":
+        query = query.replace("define ", "")
+        query = query.replace(" ", "")
 
-	if query.split()[0] == "synonym":
-		query = query.replace("synonym ", "", 1)
-		query = query.replace(" ", "")
+        returnData = dictionary.meaning(query)
+        output = ""
 
-		synonyms = dictionary.synonym(query)
-		output = ""
+        for type, definition in returnData.items():
+            for singleDefinition in definition:
+                output += type + " | " + singleDefinition + "\n"
 
-		for synonym in synonyms:
-			output += synonym + "\n"
-		
-		return output
+        return output
 
-	# Online Connections
+    if query.split()[0] == "synonym":
+        query = query.replace("synonym ", "", 1)
+        query = query.replace(" ", "")
 
-	if "wikipedia" in query:
-		query = query.replace("wikipedia", "")
-		
-		return wikipedia.summary(query)
+        synonyms = dictionary.synonym(query)
+        output = ""
 
-	if "wolfram" in query:
-		query = query.replace("wolfram alpha", "")
-		query = query.replace("wolfram", "")
+        for synonym in synonyms:
+            output += synonym + "\n"
 
-		response = wolfram.query(query)
+        return output
 
-		return next(response.results).text
+    # Online Connections
 
-	if "r/" in query:
-		query = re.sub(".*r\/", "", query)
-		query = query.replace("_", "")
-		webbrowser.open_new("https://www.reddit.com/r/" + query)
-		return "Reddit page opened."
+    if "wikipedia" in query:
+        query = query.replace("wikipedia", "")
 
-	if "google" in query:
-		query = query.replace("google", "")
-		for j in search(query, tld="co.in", num=10, stop=10, pause=2):
-			return j
+        return wikipedia.summary(query)
 
-	if "browser" in query:
-		query = query.replace("browser", "")
-		webbrowser.open_new("https://google.com")
-		return "Browser opened."
+    if "wolfram" in query:
+        query = query.replace("wolfram alpha", "")
+        query = query.replace("wolfram", "")
 
-	# Temporal
+        response = wolfram.query(query)
 
-	if query == "what is the date" or query == "what's the date" or query == "date":
-		date = datetime.datetime.now().strftime("%m/%d/%Y")
+        return next(response.results).text
 
-		return f"The date is {date} (MM/DD/YYYY)"
+    if "r/" in query:
+        query = re.sub(".*r\/", "", query)
+        query = query.replace("_", "")
+        webbrowser.open_new("https://www.reddit.com/r/" + query)
+        return "Reddit page opened."
 
-	if query == "what is the time" or query == "what's the time" or query == "what time is it" or query == "time":
-		time = datetime.datetime.now().strftime("%H:%M:%S")
+    if "google" in query:
+        query = query.replace("google", "")
+        for j in search(query, tld="co.in", num=10, stop=10, pause=2):
+            return j
 
-		return f"The time is {time}"
+    if "browser" in query:
+        query = query.replace("browser", "")
+        webbrowser.open_new("https://google.com")
+        return "Browser opened."
 
-	# Miscellanous
+    # Temporal
 
-	if "roll a " in query and "sided dice" in query:
-		query = query.replace("roll a ", "")
-		query = query.replace("sided dice", "")
+    if query == "what is the date" or query == "what's the date" or query == "date":
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
 
-		return random.randint(1, int(query))
-	
-	# Easter Eggs
+        return f"The date is {date} (MM/DD/YYYY)"
 
-	if query == "what is your name":
-		return "Zen"
-	
-	if query == "who made you":
-		return "Brian Nguyen"
+    if query == "what is the time" or query == "what's the time" or query == "what time is it" or query == "time":
+        time = datetime.datetime.now().strftime("%H:%M:%S")
 
-	if query == "what gender are you":
-		return "Animals and Spanish words have genders. I do not."
+        return f"The time is {time}"
 
-	if query == "answer to life" or query == "answer to the universe" or query == "answer to everything":
-		return "42 - The Hitchhiker's Guide to the Galaxy"
+    # Miscellanous
 
-	if query == "i am sad" or query == "i am so sad":
-		if random.randint(0, 1) == 0:
-			return "I prescribe hot chocolate and Netflix."
-		else:
-			return "Instant cure for sadness: look up pictures of cats while eating ice cream."
+    if "roll a " in query and "sided dice" in query:
+        query = query.replace("roll a ", "")
+        query = query.replace("sided dice", "")
 
-def searchHandle ():
-	set_value("Output", "Loading...")
-	set_value("Output", searchZen(get_value("Query")))
+        return random.randint(1, int(query))
 
-def copyOutput ():
-	pyperclip.copy(get_value("Output"))
+    # Easter Eggs
+
+    if query == "what is your name":
+        return "Zen"
+
+    if query == "who made you":
+        return "Brian Nguyen"
+
+    if query == "what gender are you":
+        return "Animals and Spanish words have genders. I do not."
+
+    if query == "answer to life" or query == "answer to the universe" or query == "answer to everything":
+        return "42 - The Hitchhiker's Guide to the Galaxy"
+
+    if query == "i am sad" or query == "i am so sad":
+        if random.randint(0, 1) == 0:
+            return "I prescribe hot chocolate and Netflix."
+        else:
+            return "Instant cure for sadness: look up pictures of cats while eating ice cream."
+
+
+def searchHandle():
+    dpg.set_value(output_id, "Loading...")
+    dpg.set_value(output_id, searchZen(dpg.get_value(query_id)))
+
+
+def copyOutput():
+    pyperclip.copy(dpg.get_value(output_id))
+
 
 def clear():
-	set_value("Output", "Please Enter Query")
+    dpg.set_value(output_id, "Please Enter Query")
+
 
 def openb():
-	webbrowser.open_new(get_value("Output"))
+    webbrowser.open_new(dpg.get_value(output_id))
 
-set_main_window_resizable(False)
-set_main_window_size(300, 575)
-set_main_window_title("Zen")
-set_theme("Dark 2")
 
-set_style_window_rounding(0)
+with dpg.window(label="Search Window", pos=[0, 0], no_close=True, no_collapse=True, no_move=True, no_resize=True, width=284, height=268):
+    dpg.add_input_text(label="", width=267, on_enter=True, callback=searchHandle, id=query_id)
+    dpg.add_button(label="Run", callback=searchHandle)
+    dpg.add_button(label="Copy Result", callback=copyOutput)
+    dpg.add_button(label="Clear Result", callback=clear)
+    dpg.add_button(label="Open Result", callback=openb)
 
-with window("Search Window", no_close = True, no_collapse = True, no_move = True, no_resize = True, width = 284, height = 268):
-	add_input_text("Query", label = "", width = 267, on_enter = True, callback = searchHandle)
-	add_button("Run", callback = searchHandle)
-	add_button("Copy Result", callback = copyOutput)
-	add_button("Clear Result", callback = clear)
-	add_button("Open Result", callback = openb)
+with dpg.window(label="Results Window", pos=[0, 268], no_close=True, no_collapse=True, no_move=True, no_resize=True, width=284, height=268):
+    dpg.add_text("Output", wrap=250)
 
-with window("Results Window", no_close = True, no_collapse = True, no_move = True, no_resize = True, width = 284, height = 268):
-	add_text("Output", wrap = 250)
+dpg.set_value(output_id, "Please Enter Query")
 
-set_window_pos("Search Window", 0, 0)
-set_window_pos("Results Window", 0, 268)
-set_value("Output", "Please Enter Query")
-
-start_dearpygui()
+dpg.setup_viewport()
+dpg.set_viewport_resizable(False)
+dpg.set_viewport_width(300)
+dpg.set_viewport_height(575)
+dpg.set_viewport_title("Zen")
+dpg.start_dearpygui()
